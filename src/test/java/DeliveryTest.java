ff1f12a820e070c826659a3a5b4b4d3cf7ea82c8
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-20 22:36:18 -0700

    appveyor

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,13 +1,10 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
 import java.time.Duration;
-
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$x;
